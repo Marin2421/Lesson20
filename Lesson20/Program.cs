b3c6fd8496f1d54516b9c12ca38a23af4ad94be0
ASUS <ASUS@DESKTOP-QOB7I2F>
2024-05-27 18:10:39 +0300

    Добавьте файлы проекта.

---
+++ Lesson20/Program.cs
@@ -0,0 +1,28 @@
+﻿//Тема 20. Локальные функции. Домашнее задание.
+
+//1
+//Console.Write("Введите первое число:");
+//int n = int.Parse(Console.ReadLine());
+//Console.Write("Введите второе число:");
+//int m = int.Parse(Console.ReadLine());
+//Console.WriteLine(Add(n, m));
+//int Add(int a, int b)
+//{
+//    return a + b;
+//}
+
+//2
+//Random random = new Random();
+//int[] mas = new int[random.Next(5,11)];
+//for (int i = 0; i < mas.Length; i++)
+//{
+//	mas[i] = random.Next(10, 100);
+//	Console.Write(mas[i]+" ");
+//}
+//Console.WriteLine($"Среднее арифметическое:{Avg(mas):F2}");
+//double Avg(int[] mas)
+//{
+//    double s = 0;
+//	for (int i = 0; i < mas.Length; i++) s += mas[i];
+//	return s / mas.Length;
+//}
